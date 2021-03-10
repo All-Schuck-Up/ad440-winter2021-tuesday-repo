57c51f2eef67ebf3f1c18c287f25125b65fb7bb0
All-Schuck-Up <schuckrw@gmail.com>
2021-03-09 17:56:34 -0800

    caching update

--- functions/HttpTriggerUserIdTask/__init__.py
+++ functions/HttpTriggerUserIdTask/__init__.py
@@ -10,7 +10,7 @@ import datetime
 
 # GLOBAL VARIABLES
 CACHE_TOGGLE = os.environ.get('CACHE_TOGGLE')
-TASKS_CACHE = b'users:all'
+TASKS_CACHE = b'users:userId:tasks:all'
 
 # to handle datetime with JSON (NOT WORKING CURRENTLY)
 # It serialize datetime by converting it into string
@@ -49,19 +49,20 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     # Setup Redis server
     r = setupRedis()
+    logging.debug('Connected to redis server!')
 
     try:
         # Return results according to the method
         if method == "GET":
             logging.debug("trying to get one user with id {} all tasks".format(userId))
-            all_tasks_by_userId = getUserTasks(conn, userId)
+            all_tasks_by_userId = getUserTasks(conn, userId, r)
             logging.debug("Users retrieved successfully!")
             return all_tasks_by_userId
 
         elif method == "POST":
             logging.debug("trying to add one task to tasks")
             task_req_body = req.get_json()
-            new_task_id = addUserTask(conn, task_req_body, userId)
+            new_task_id = addUserTask(conn, task_req_body, userId, r)
             logging.debug("task added successfully!")
             return new_task_id
 
@@ -76,34 +77,52 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         conn.close()
         logging.debug('Connection to DB closed')
 
-def getUserTasks(conn, userId):
-    with conn.cursor() as cursor:
-        logging.debug('Inside def: getUsersTasks')
-        cursor.execute(
-            "SELECT taskId, taskUserId, title, taskDescription FROM tasks WHERE taskUserId={}".format(userId)
-        )
-        logging.debug('Fetching all tasks for user: ' + userId)
-        tasks = list(cursor.fetchall())
+def getUserTasks(conn, userId, r):
+    try: 
+        cache = getTasksCache(r)
+    except TypeError as err:
+        logging.debug(err.args[0])
 
-        # Clean up for json format
-        task_data = [tuple(task) for task in tasks]
-
-        # Empty data list
-        tasks = []
-        columns = [column[0] for column in cursor.description]
-
-        for task in task_data:
-            tasks.append(dict(zip(columns, task)))
-
-        logging.debug('Tasks retrieved')
-
+    if cache:
+        logging.debug('Tasks data found in cache and being returned')
         return func.HttpResponse(
-            json.dumps(tasks),
+            cache.decode('utf-8'),
             status_code=200,
             mimetype='application/json'
         )
+    else:
+        logging.debug('Data not found in cache searching database...')
+        with conn.cursor() as cursor:
+            logging.debug('Inside def: getUsersTasks')
+            cursor.execute(
+                "SELECT taskId, taskUserId, title, taskDescription FROM tasks WHERE taskUserId={}".format(userId)
+            )
+            logging.debug('Fetching all tasks for user: ' + userId)
+            tasks = list(cursor.fetchall())
 
-def addUserTask(conn, task_req_body, userId):
+            # Clean up for json format
+            task_data = [tuple(task) for task in tasks]
+
+            # Empty data list
+            tasks = []
+            columns = [column[0] for column in cursor.description]
+
+            for task in task_data:
+                tasks.append(dict(zip(columns, task)))
+
+            logging.debug('Tasks retrieved')
+
+            # if CACHE_TOGGLE == True: save tasks to cache
+            if(CACHE_TOGGLE == True):
+                cacheTasks(r, tasks)
+
+            return func.HttpResponse(
+                json.dumps(tasks),
+                status_code=200,
+                mimetype='application/json'
+            )
+
+def addUserTask(conn, task_req_body, userId, r):
     # Verify required fields
     logging.debug('Verifying required fields')
     try:
@@ -112,6 +131,7 @@ def addUserTask(conn, task_req_body, userId):
     except AssertionError as err:
         logging.error("New user request body did not contain the necessary fields!")
         return func.HttpResponse(err.args[0], status_code=400)
+
     logging.debug("New task request body contains all the necessary fields!")
 
     with conn.cursor() as cursor:
@@ -135,6 +155,10 @@ def addUserTask(conn, task_req_body, userId):
         # Get the user id from cursor 
         taskId = cursor.fetchval()
         logging.info(taskId)
+
+        # Clear cache 
+        clearTasksCache(r)
+
         logging.debug('task with id {} added'.format(taskId))
         return func.HttpResponse(
             json.dumps({"taskId": taskId}), 
@@ -155,3 +179,27 @@ def setupRedis():
         password= REDIS_KEY,
         ssl= True
     )
+
+def cacheTasks(r, tasks):
+    if(CACHE_TOGGLE == True):
+        try:
+            logging.debug('Caching Tasks in redis...')
+            r.set(TASKS_CACHE, json.dump(tasks), ex=1200)
+            logging.debug('Tasks cached successfully!')
+        except Exception as err:
+            logging.debug('Caching failed')
+            logging.debug(err.args[0])
+
+def getTasksCache(r):
+    if(CACHE_TOGGLE == True):
+        logging.debug('Checking cache for tasks...')
+        try:
+            cache = r.get(TASKS_CACHE)
+            return cache
+        except ExceptionWithStatusCode as err:
+            logging.debug('Failed to fetch tasks from cache')
+            return func.HttpResponse(str(err), status_code=err.status_code)
+
+def clearTasksCache(r):
+    r.delete(TASKS_CACHE)
+    logging.debug('Tasks cache cleared successfully')
